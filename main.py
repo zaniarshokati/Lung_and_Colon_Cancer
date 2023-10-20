b64dc813852afec4a1937a3c0855b5a9a69321c8
Zaniar Shokati <zaniar.shokati@gmail.com>
2023-10-20 11:12:00 +0200

    black format

--- main.py
+++ main.py
@@ -1,52 +1,90 @@
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import Recall, Precision, AUC, RootMeanSquaredError
-from sklearn.metrics import f1_score
 import os
 
 import utilities
+
+
 # Generate data paths with labels
 class Application:
     def __init__(self) -> None:
         self.data_handler = utilities.HandleData()
         self.visualizer = utilities.Visualization()
         self.model_handler = utilities.HandleModel()
-        self.DATA_DIR = 'data'
-        self.SAMPLE_SIZE=5000
-        self.TEST_BATCH_SIZE=50  
-        self.TEST_STEPS=50
+        self.DATA_DIR = "data"
+        self.SAMPLE_SIZE = 5000
+        self.TEST_BATCH_SIZE = 50
+        self.TEST_STEPS = 50
 
     def main(self):
         df = self.data_handler.load_data(self.DATA_DIR)
         df = self.data_handler.balance_dataset(df, self.SAMPLE_SIZE)
 
         train_df, test_df, valid_df = self.data_handler.split_data(df)
-        train_generator, test_generator, valid_generator = self.data_handler.create_data_generators(train_df, test_df, valid_df)
-        classes=list(train_generator.class_indices.keys())
-        
+        (
+            train_generator,
+            test_generator,
+            valid_generator,
+        ) = self.data_handler.create_data_generators(train_df, test_df, valid_df)
+        classes = list(train_generator.class_indices.keys())
+
         # self.visualizer.show_image_samples(train_generator)
-        num_classes=len(classes)
+        num_classes = len(classes)
         model = self.model_handler.cerate_model(num_classes)
-        model.compile(Adam(learning_rate=.001), loss='categorical_crossentropy', metrics=['accuracy', Precision(), Recall(), AUC(), 'mae', 'mse', RootMeanSquaredError()])
+        model.compile(
+            Adam(learning_rate=0.001),
+            loss="categorical_crossentropy",
+            metrics=[
+                "accuracy",
+                Precision(),
+                Recall(),
+                AUC(),
+                "mae",
+                "mse",
+                RootMeanSquaredError(),
+            ],
+        )
         model.summary()
 
-        model_name = 'Xception+MobileNet'
+        model_name = "Xception+MobileNet"
         epochs = 1
-        history = self.model_handler.train_model(train_generator, valid_generator, model, model_name, epochs)
+        history = self.model_handler.train_model(
+            train_generator, valid_generator, model, model_name, epochs
+        )
 
         self.visualizer.plot_training(history)
 
-        save_dir=r'./'
-        subject='best'
-        acc=model.evaluate( test_generator, batch_size=self.TEST_BATCH_SIZE, verbose=1, steps=self.TEST_STEPS, return_dict=False)[1]*100
-        msg=f'accuracy on the test set is {acc:5.2f} %'
-        self.visualizer.print_in_color(msg, (0,255,0),(55,65,80))
-        save_id=str (model_name +  '-' + subject +'-'+ str(acc)[:str(acc).rfind('.')+3] + '.h5')
-        save_loc=os.path.join(save_dir, save_id)
+        save_dir = r"./"
+        subject = "best"
+        acc = (
+            model.evaluate(
+                test_generator,
+                batch_size=self.TEST_BATCH_SIZE,
+                verbose=1,
+                steps=self.TEST_STEPS,
+                return_dict=False,
+            )[1]
+            * 100
+        )
+        msg = f"accuracy on the test set is {acc:5.2f} %"
+        self.visualizer.print_in_color(msg, (0, 255, 0), (55, 65, 80))
+        save_id = str(
+            model_name
+            + "-"
+            + subject
+            + "-"
+            + str(acc)[: str(acc).rfind(".") + 3]
+            + ".h5"
+        )
+        save_loc = os.path.join(save_dir, save_id)
         model.save(save_loc)
 
-        print_code=0
-        predictions=model.predict(test_generator) 
-        self.visualizer.print_info( test_generator, predictions, print_code, save_dir, subject ) 
+        print_code = 0
+        predictions = model.predict(test_generator)
+        self.visualizer.print_info(
+            test_generator, predictions, print_code, save_dir, subject
+        )
+
 
 if __name__ == "__main__":
     app = Application()
--- utilities.py
+++ utilities.py
@@ -6,16 +6,29 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import seaborn as sns
-sns.set_style('darkgrid')
+
+sns.set_style("darkgrid")
 
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import Xception, MobileNet
-from tensorflow.keras.layers import Dense, Activation,Dropout,Conv2D, MaxPooling2D,BatchNormalization, Flatten, Add, AveragePooling2D, Input, ZeroPadding2D,Concatenate
+from tensorflow.keras.layers import (
+    Dense,
+    Activation,
+    Dropout,
+    Conv2D,
+    MaxPooling2D,
+    BatchNormalization,
+    Flatten,
+    Add,
+    AveragePooling2D,
+    Input,
+    ZeroPadding2D,
+    Concatenate,
+)
 from tensorflow.keras.models import Model
 from sklearn.metrics import confusion_matrix, classification_report
 from tensorflow.python.keras.callbacks import EarlyStopping
-
 
 
 class HandleData:
@@ -23,34 +36,56 @@ class HandleData:
         self.train_size = 0.8
         self.test_size = 0.1
         self.data = []
-   
+
     def load_data(self, DATA_DIR):
         for root, dirs, files in os.walk(DATA_DIR):
             for cls in dirs:
                 if cls not in ["lung_image_sets", "colon_image_sets"]:
                     class_path = os.path.join(root, cls)
                     for f in os.listdir(class_path):
-                        self.data.append({'filepaths': os.path.join(class_path, f), 'labels': cls})
+                        self.data.append(
+                            {"filepaths": os.path.join(class_path, f), "labels": cls}
+                        )
 
         df = pd.DataFrame(self.data)
         # print(df['labels'].value_counts())
         return df
-    
+
     def balance_dataset(self, df, sample_size):
-        sample_list=[]
-        group = df.groupby('labels')
-        sample_list = [group.get_group(label).sample(sample_size, replace=False, random_state=123, axis=0) for label in df['labels'].unique()]
+        sample_list = []
+        group = df.groupby("labels")
+        sample_list = [
+            group.get_group(label).sample(
+                sample_size, replace=False, random_state=123, axis=0
+            )
+            for label in df["labels"].unique()
+        ]
         df = pd.concat(sample_list, axis=0).reset_index(drop=True)
         # print(len(df))
         return df
-    
+
     def split_data(self, df):
-        train_df, test_valid_df = train_test_split(df, train_size=self.train_size, shuffle=True, random_state=123)
-        test_df, valid_df = train_test_split(test_valid_df, train_size=self.test_size / (1 - self.train_size), shuffle=True, random_state=123)
+        train_df, test_valid_df = train_test_split(
+            df, train_size=self.train_size, shuffle=True, random_state=123
+        )
+        test_df, valid_df = train_test_split(
+            test_valid_df,
+            train_size=self.test_size / (1 - self.train_size),
+            shuffle=True,
+            random_state=123,
+        )
         # print('train_df length:', len(train_df), 'test_df length:', len(test_df), 'valid_df length:', len(valid_df))
-        return train_df,test_df,valid_df
-    
-    def create_data_generators(self, train_df, test_df, valid_df, img_size=(224, 224), batch_size=32, test_batch_size=50):
+        return train_df, test_df, valid_df
+
+    def create_data_generators(
+        self,
+        train_df,
+        test_df,
+        valid_df,
+        img_size=(224, 224),
+        batch_size=32,
+        test_batch_size=50,
+    ):
         # Custom preprocessing function to scale pixel values between -1 and +1
         def scale_image(img):
             return img / 127.5 - 1
@@ -63,38 +98,39 @@ class HandleData:
         # Create data generators
         train_generator = datagen.flow_from_dataframe(
             train_df,
-            x_col='filepaths',
-            y_col='labels',
+            x_col="filepaths",
+            y_col="labels",
             target_size=img_size,
-            class_mode='categorical',
-            color_mode='rgb',
+            class_mode="categorical",
+            color_mode="rgb",
             shuffle=True,
             batch_size=batch_size,
         )
 
         test_generator = datagen.flow_from_dataframe(
             test_df,
-            x_col='filepaths',
-            y_col='labels',
+            x_col="filepaths",
+            y_col="labels",
             target_size=img_size,
-            class_mode='categorical',
-            color_mode='rgb',
+            class_mode="categorical",
+            color_mode="rgb",
             shuffle=False,  # No shuffling for testing data
             batch_size=test_batch_size,  # Test the entire dataset in one batch
         )
 
         valid_generator = datagen.flow_from_dataframe(
             valid_df,
-            x_col='filepaths',
-            y_col='labels',
+            x_col="filepaths",
+            y_col="labels",
             target_size=img_size,
-            class_mode='categorical',
-            color_mode='rgb',
+            class_mode="categorical",
+            color_mode="rgb",
             shuffle=True,
             batch_size=batch_size,
         )
-        
+
         return train_generator, test_generator, valid_generator
+
 
 class Visualization:
     def __init__(self) -> None:
@@ -104,7 +140,7 @@ class Visualization:
         test_dict = train_generator.class_indices
         classes = list(test_dict.keys())
 
-           # Get a sample batch from the generator
+        # Get a sample batch from the generator
         images, labels = next(train_generator)
 
         num_samples = len(labels)
@@ -116,21 +152,21 @@ class Visualization:
             plt.subplot(num_rows, 5, i + 1)
             image = (images[i] + 1) / 2  # Scale images between 0 and 1
             plt.imshow(image)
-            
+
             index = np.argmax(labels[i])
             class_name = classes[index]
-            plt.title(class_name, color='blue', fontsize=12)
-            plt.axis('off')
+            plt.title(class_name, color="blue", fontsize=12)
+            plt.axis("off")
 
         plt.tight_layout()
         plt.show()
 
     def plot_training(self, tr_data):
         # Extract training and validation data
-        tacc = tr_data.history['accuracy']
-        tloss = tr_data.history['loss']
-        vacc = tr_data.history['val_accuracy']
-        vloss = tr_data.history['val_loss']
+        tacc = tr_data.history["accuracy"]
+        tloss = tr_data.history["loss"]
+        vacc = tr_data.history["val_accuracy"]
+        vloss = tr_data.history["val_loss"]
 
         # Get the number of epochs directly
         Epochs = range(1, len(tacc) + 1)
@@ -142,27 +178,27 @@ class Visualization:
         acc_highest = max(vacc)
 
         # Plot the data
-        plt.style.use('fivethirtyeight')
+        plt.style.use("fivethirtyeight")
         fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20, 8))
 
         # Plot training and validation loss
-        sc_label = f'best epoch = {index_loss}'
-        axes[0].plot(Epochs, tloss, 'r', label='Training loss')
-        axes[0].plot(Epochs, vloss, 'g', label='Validation loss')
-        axes[0].scatter(index_loss, val_lowest, s=150, c='blue', label=sc_label)
-        axes[0].set_title('Training and Validation Loss')
-        axes[0].set_xlabel('Epochs')
-        axes[0].set_ylabel('Loss')
+        sc_label = f"best epoch = {index_loss}"
+        axes[0].plot(Epochs, tloss, "r", label="Training loss")
+        axes[0].plot(Epochs, vloss, "g", label="Validation loss")
+        axes[0].scatter(index_loss, val_lowest, s=150, c="blue", label=sc_label)
+        axes[0].set_title("Training and Validation Loss")
+        axes[0].set_xlabel("Epochs")
+        axes[0].set_ylabel("Loss")
         axes[0].legend()
 
         # Plot training and validation accuracy
-        vc_label = f'best epoch = {index_acc}'
-        axes[1].plot(Epochs, tacc, 'r', label='Training Accuracy')
-        axes[1].plot(Epochs, vacc, 'g', label='Validation Accuracy')
-        axes[1].scatter(index_acc, acc_highest, s=150, c='blue', label=vc_label)
-        axes[1].set_title('Training and Validation Accuracy')
-        axes[1].set_xlabel('Epochs')
-        axes[1].set_ylabel('Accuracy')
+        vc_label = f"best epoch = {index_acc}"
+        axes[1].plot(Epochs, tacc, "r", label="Training Accuracy")
+        axes[1].plot(Epochs, vacc, "g", label="Validation Accuracy")
+        axes[1].scatter(index_acc, acc_highest, s=150, c="blue", label=vc_label)
+        axes[1].set_title("Training and Validation Accuracy")
+        axes[1].set_xlabel("Epochs")
+        axes[1].set_ylabel("Accuracy")
         axes[1].legend()
 
         plt.tight_layout()  # Properly adjust subplot layout
@@ -198,17 +234,19 @@ class Visualization:
         if print_code != 0:
             if errors > 0:
                 r = min(print_code, errors)
-                header = '{0:^28s}{1:^28s}{2:^28s}{3:^16s}'.format('Filename', 'Predicted Class', 'True Class', 'Probability')
+                header = "{0:^28s}{1:^28s}{2:^28s}{3:^16s}".format(
+                    "Filename", "Predicted Class", "True Class", "Probability"
+                )
                 self.print_in_color(header, (0, 255, 0), (55, 65, 80))
 
                 for i in range(r):
                     split1, fname = os.path.split(error_list[i])
                     split2, split1 = os.path.split(split1)
-                    filename = f'{split1}/{fname}'
-                    msg = f'{filename:^28s}{pred_class[i]:^28s}{true_class[i]:^28s}    {prob_list[i]:.4f}'
+                    filename = f"{split1}/{fname}"
+                    msg = f"{filename:^28s}{pred_class[i]:^28s}{true_class[i]:^28s}    {prob_list[i]:.4f}"
                     self.print_in_color(msg, (255, 255, 255), (55, 65, 60))
             else:
-                msg = 'With accuracy of 100%, there are no errors to print'
+                msg = "With accuracy of 100%, there are no errors to print"
                 self.print_in_color(msg, (0, 255, 0), (55, 65, 80))
 
         if errors > 0:
@@ -222,13 +260,13 @@ class Visualization:
                     plot_class.append(value)
 
             fig = plt.figure(figsize=(10, len(plot_class) / 3))
-            plt.style.use('fivethirtyeight')
+            plt.style.use("fivethirtyeight")
 
             for i in range(len(plot_class)):
                 c, x = plot_class[i], plot_bar[i]
                 plt.barh(c, x)
 
-            plt.title('Errors by Class on Test Set')
+            plt.title("Errors by Class on Test Set")
 
         y_true = np.array(labels)
         y_pred = np.array(y_pred)
@@ -243,7 +281,7 @@ class Visualization:
                 fig_width, fig_height = int(length * 0.5), int(length * 0.5)
 
             plt.figure(figsize=(fig_width, fig_height))
-            sns.heatmap(cm, annot=True, vmin=0, fmt='g', cmap='Blues', cbar=False)
+            sns.heatmap(cm, annot=True, vmin=0, fmt="g", cmap="Blues", cbar=False)
             plt.xticks(np.arange(length) + 0.5, classes, rotation=90)
             plt.yticks(np.arange(length) + 0.5, classes, rotation=0)
             plt.xlabel("Predicted")
@@ -255,24 +293,25 @@ class Visualization:
         print("Classification Report:\n----------------------\n", clr)
 
     def print_in_color(self, text, foreground_color, background_color):
-            # Extract RGB components for the foreground and background colors
-            r_fg, g_fg, b_fg = foreground_color
-            r_bg, g_bg, b_bg = background_color
+        # Extract RGB components for the foreground and background colors
+        r_fg, g_fg, b_fg = foreground_color
+        r_bg, g_bg, b_bg = background_color
 
-            # Define the color format using ANSI escape codes
-            color_format = f'\33[38;2;{r_fg};{g_fg};{b_fg};48;2;{r_bg};{g_bg};{b_bg}m'
+        # Define the color format using ANSI escape codes
+        color_format = f"\33[38;2;{r_fg};{g_fg};{b_fg};48;2;{r_bg};{g_bg};{b_bg}m"
 
-            # Print the text with the specified color format
-            print(color_format + text)
-            
-            # Reset the color to default (black text on a white background)
-            print('\33[0m')
-            return
+        # Print the text with the specified color format
+        print(color_format + text)
+
+        # Reset the color to default (black text on a white background)
+        print("\33[0m")
+        return
+
 
 class HandleModel:
     def cerate_model(self, num_classes):
-        mobilenet = MobileNet(weights='imagenet', include_top=False)
-        xception = Xception(weights='imagenet', include_top=False)
+        mobilenet = MobileNet(weights="imagenet", include_top=False)
+        xception = Xception(weights="imagenet", include_top=False)
 
         # freeze the layers
         for layer in mobilenet.layers:
@@ -281,7 +320,7 @@ class HandleModel:
             layer.trainable = False
 
         # create a single input layer
-        input_layer = Input(shape=(224,224,3))
+        input_layer = Input(shape=(224, 224, 3))
 
         # connect the input layer to both models
         x1 = mobilenet(input_layer)
@@ -292,32 +331,34 @@ class HandleModel:
 
         # add a few dense layers for the final prediction
         flat = Flatten()(concatenated)
-        dense = Dense(units=1024, activation='relu')(flat)
-        dense = Dense(512, activation='relu')(dense)
-        dense = Dense(units=256, activation='relu')(dense)
-        predictions = Dense(num_classes, activation='softmax')(dense)
+        dense = Dense(units=1024, activation="relu")(flat)
+        dense = Dense(512, activation="relu")(dense)
+        dense = Dense(units=256, activation="relu")(dense)
+        predictions = Dense(num_classes, activation="softmax")(dense)
         # compile the model
         # this is the model we will train
         model = Model(inputs=input_layer, outputs=predictions)
         return model
-    
+
     def train_model(self, train_generator, valid_generator, model, model_name, epochs):
-        early_stop = EarlyStopping(monitor='val_loss', patience=15, mode='min', verbose=1)
+        early_stop = EarlyStopping(
+            monitor="val_loss", patience=15, mode="min", verbose=1
+        )
         # Create a list of callbacks for training
         callbacks = [
-                LearningRateAdjustment(
-                    model=model,
-                    patience=9,
-                    stop_patience=3,
-                    threshold=0.9,
-                    factor=0.5,
-                    dwell=True,
-                    model_name=model_name,
-                    freeze=False,
-                    initial_epoch=0
-                ), 
-                early_stop
-            ]
+            LearningRateAdjustment(
+                model=model,
+                patience=9,
+                stop_patience=3,
+                threshold=0.9,
+                factor=0.5,
+                dwell=True,
+                model_name=model_name,
+                freeze=False,
+                initial_epoch=0,
+            ),
+            early_stop,
+        ]
 
         # Set the total number of epochs for printing in the LearningRateAdjustment callback
         LearningRateAdjustment.tepochs = epochs
@@ -331,13 +372,25 @@ class HandleModel:
             validation_data=valid_generator,  # Validation data
             validation_steps=None,  # Number of validation steps
             shuffle=False,  # Disable shuffling of training data
-            initial_epoch=0  # Initial epoch
+            initial_epoch=0,  # Initial epoch
         )
-        
+
         return history
 
+
 class LearningRateAdjustment(keras.callbacks.Callback):
-    def __init__(self, model, patience, stop_patience, threshold, factor, dwell, model_name, freeze, initial_epoch):
+    def __init__(
+        self,
+        model,
+        patience,
+        stop_patience,
+        threshold,
+        factor,
+        dwell,
+        model_name,
+        freeze,
+        initial_epoch,
+    ):
         super(LearningRateAdjustment, self).__init__()
 
         self.model = model
@@ -351,7 +404,7 @@ class LearningRateAdjustment(keras.callbacks.Callback):
         self.lowest_vloss = np.inf
         self.initial_epoch = initial_epoch
         self.best_weights = self.model.get_weights()
-        
+
         # Print a message indicating the start of training
         msg = f"Starting training using base model {model_name} with {'frozen' if freeze else 'trainable'} layers.\n"
         self.print_in_color(msg, (244, 252, 3), (55, 65, 80))
@@ -364,20 +417,28 @@ class LearningRateAdjustment(keras.callbacks.Callback):
         # Print a header if it's the initial epoch or there's a reset
         if epoch == self.initial_epoch or LearningRateAdjustment.reset:
             LearningRateAdjustment.reset = False
-            msg = '{0:^8s}{1:^10s}{2:^9s}{3:^9s}{4:^9s}{5:^9s}{6:^9s}{7:^11s}{8:^8s}'.format(
-                'Epoch', 'Loss', 'Accuracy', 'V_loss', 'V_acc', 'LR', 'Next LR', 'Monitor', 'Duration'
+            msg = "{0:^8s}{1:^10s}{2:^9s}{3:^9s}{4:^9s}{5:^9s}{6:^9s}{7:^11s}{8:^8s}".format(
+                "Epoch",
+                "Loss",
+                "Accuracy",
+                "V_loss",
+                "V_acc",
+                "LR",
+                "Next LR",
+                "Monitor",
+                "Duration",
             )
             self.print_in_color(msg, (244, 252, 3), (55, 65, 80))
 
         # Get current learning rate, validation loss, and accuracy
         lr = float(tf.keras.backend.get_value(self.model.optimizer.lr))
         current_lr = lr
-        v_loss = logs.get('val_loss')
-        acc = logs.get('accuracy')
-        v_acc = logs.get('val_accuracy')
-        loss = logs.get('loss')
+        v_loss = logs.get("val_loss")
+        acc = logs.get("accuracy")
+        v_acc = logs.get("val_accuracy")
+        loss = logs.get("loss")
 
-        monitor = 'accuracy' if acc < self.threshold else 'val_loss'
+        monitor = "accuracy" if acc < self.threshold else "val_loss"
 
         if acc < self.threshold:
             if acc > self.highest_tracc:
@@ -424,11 +485,11 @@ class LearningRateAdjustment(keras.callbacks.Callback):
 
             # if acc > self.highest_tracc:
             #     self.highest_tracc = acc
-            msg = f'{str(epoch + 1):^3s}/{str(LearningRateAdjustment.tepochs):4s} {loss:^9.3f}{acc*100:^9.3f}{v_loss:^9.5f}{v_acc*100:^9.3f}{current_lr:^9.5f}{self.lr:^9.5f}{monitor:^11s}{duration:^8.2f}'
+            msg = f"{str(epoch + 1):^3s}/{str(LearningRateAdjustment.tepochs):4s} {loss:^9.3f}{acc*100:^9.3f}{v_loss:^9.5f}{v_acc*100:^9.3f}{current_lr:^9.5f}{self.lr:^9.5f}{monitor:^11s}{duration:^8.2f}"
             self.print_in_color(msg, (0, 67, 54), (55, 65, 80))
 
         if self.stop_count > self.stop_patience - 1:
-            msg = f'Training halted at epoch {epoch + 1} after {self.stop_patience} adjustments of learning rate with no improvement.'
+            msg = f"Training halted at epoch {epoch + 1} after {self.stop_patience} adjustments of learning rate with no improvement."
             self.print_in_color(msg, (0, 255, 0), (55, 65, 80))
             self.model.stop_training = True
 
@@ -438,7 +499,7 @@ class LearningRateAdjustment(keras.callbacks.Callback):
     def print_in_color(self, text_msg, foreground_color, background_color):
         r_fg, g_fg, b_fg = foreground_color
         r_bg, g_bg, b_bg = background_color
-        color_format = f'\33[38;2;{r_fg};{g_fg};{b_fg};48;2;{r_bg};{g_bg};{b_bg}m'
+        color_format = f"\33[38;2;{r_fg};{g_fg};{b_fg};48;2;{r_bg};{g_bg};{b_bg}m"
         print(color_format + text_msg)
-        
-        print('\33[0m')
+
+        print("\33[0m")
